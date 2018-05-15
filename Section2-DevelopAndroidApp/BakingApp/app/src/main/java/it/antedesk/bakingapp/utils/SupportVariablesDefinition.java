f47a9cd68bb8c874ae171fc60373d082aa7fc2e0
Antedesk <antedesk@gmail.com>
2018-05-16 00:22:42 +0200

    added support library and variables

--- Section2-DevelopAndroidApp/BakingApp/app/src/main/java/it/antedesk/bakingapp/utils/SupportVariablesDefinition.java
+++ Section2-DevelopAndroidApp/BakingApp/app/src/main/java/it/antedesk/bakingapp/utils/SupportVariablesDefinition.java
@@ -6,4 +6,9 @@ package it.antedesk.bakingapp.utils;
 
 public final class SupportVariablesDefinition {
 
+    public static final String RECIPES_DATASOURCE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+
+    //variable for logging
+    public static final String HOME_ACTIVITY_LOADING = "HOME_ACTIVITY_LOADING";
+
 }
